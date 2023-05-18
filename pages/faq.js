c24bce96bb369833cd93452b24e9123214790223
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-05-18 09:27:44 -0300

    primeiro commit

---
+++ pages/faq.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Alura Cases - Página de Perguntas FAQ</h1>
+      <Link href='/'>Ir para a HomePage</Link>
+    </div>
+  );
+}
