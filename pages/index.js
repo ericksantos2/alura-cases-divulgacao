a0c87cf2d29005e269627a1174ba98e8318291cb
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-05-18 10:30:24 -0300

    removi tudo que estava comentado

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,3 @@
 import HomeScreen from "../src/screens/HomeScreen";
 
 export default HomeScreen;
-
-// import Link from 'next/link';
-// import Footer from '../src/components/patterns/Footer';
-
-// function Title({ children, as }) {
-//   const Tag = as || 'h1';
-//   return (
-//     <>
-//       <Tag>{children}</Tag>
-//       <style jsx>{`
-//         ${Tag} {
-//           color: red;
-//         }
-//       `}</style>
-//     </>
-//   );
-// }
-
-// export default function HomePage() {
-//   return (
-//     <div>
-//       <Title>Alura Cases - Home</Title>
-//       <Link href='/faq'>Ir para a página do FAQ</Link>
-//       <Footer />
-//     </div>
-//   );
-// }
