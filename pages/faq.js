e36aab78eb09f72d5b4aa5c437071cc1c6307094
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-05-18 10:30:14 -0300

    adicionei uma integração a "api" para carregar as perguntas e respostas

--- pages/faq.js
+++ pages/faq.js
@@ -1,10 +1,42 @@
+import Head from "next/head";
 import Link from "next/link";
 
-export default function HomePage() {
+export async function getStaticProps() {
+  const FAQ_API_URL='https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+  const faq = await fetch(FAQ_API_URL)
+      .then((respostaDoServidor) => {
+        return respostaDoServidor.json()
+      })
+      .then((resposta) => {
+        return resposta;
+      })
+  return {
+    props: {
+      faq
+    },
+  };
+}
+
+export default function FaqPage({ faq }) {
   return (
     <div>
+      <Head>
+        <title>
+          FAQ - Alura Cases Campanha
+        </title>
+      </Head>
       <h1>Alura Cases - Página de Perguntas FAQ</h1>
       <Link href='/'>Ir para a HomePage</Link>
+      <ul>
+        {faq.map(({ answer, question }, index) => (
+          <li key={index}>
+            <article>
+              <h2>{question}</h2>
+              <p>{answer}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
